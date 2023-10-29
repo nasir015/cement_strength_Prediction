e10547c2d94bf75c40d8865794235b2c8b3f9558
nasir015 <nasir1515@student.nstu.edu.bd>
2023-10-29 10:24:08 +0600

    first commit

---
+++ tamplate.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+
+
+
+
+
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/__init__.py",
+    f"src/components/__init__.py",
+    f"src/components/Model_selection.ipynb",
+    f"src/utils/__init__.py",
+    f"src/utils/common.py",
+    f"src/pipeline/__init__.py",
+    f"src/pipeline/logging.py",
+    f"src/pipeline/Exception.py",
+    "requirements.txt",
+    "setup.py",
+    "EDA/EDA.ipynb",
+    "Data/data_retrive_from_db.py",
+    "Data/data_upload_into_db.py",
+    "Log/log.txt",
+    "README.md",
+    
+
+
+]
+
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+
+
+    else:
+        print(f"WARNING: {filepath} already exists! No action taken.")
